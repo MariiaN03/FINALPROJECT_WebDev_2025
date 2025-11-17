f35a61b46acb5a6130d2d65f28eae7996989516d
MariiaN03 <553574@student.saxion.nl>
2025-11-17 00:20:20 -0500

    MainPage_PhotoReel
    PhotoReel section blockout done

---
+++ js/MainPage.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const slides = document.querySelectorAll('.backgroundPhoto');
+    if (!slides.length) return;
+
+    let index = 0;
+
+    setInterval(() => {
+        slides[index].classList.remove('active');
+        index = (index + 1) % slides.length;
+        slides[index].classList.add('active');
+    }, 5000); // 5 секунд
+});
