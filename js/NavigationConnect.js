9da1bde4bb11e74d3c7b5479d1c8d6af06d08015
MariiaN03 <553574@student.saxion.nl>
2025-12-10 04:03:34 -0500

    Navigation2
    Change navigation Code

--- js/NavigationConnect.js
+++ js/NavigationConnect.js
@@ -1,8 +1,14 @@
 fetch('NavigationMenu.html')
   .then(response => response.text())
   .then(html => {
-    document.getElementById('NavigationBar').innerHTML = html;
+    const container = document.getElementById('NavigationBar');
+    container.innerHTML = html;
+
+    // здесь элементы уже есть в DOM → можно инициализировать меню
+    if (window.initMenu) {
+      window.initMenu();
+    }
   })
   .catch(err => {
-    console.error('Ошибка загрузки header.html:', err);
+    console.error('Ошибка загрузки NavigationMenu.html:', err);
   });
